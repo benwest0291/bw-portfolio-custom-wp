bd90994f00857fc41886a8fd95f7f27b59abce96
Ben West <benwest@Bens-MacBook-Pro.local>
2023-08-18 20:47:12 +0100

    linked to hosting and deleted test commits

--- header.php
+++ header.php
@@ -31,9 +31,6 @@ if (isset($menuLocations['header'])) {
         gtag('config', 'G-DHN2E0YZ2V');
     </script>
     <!-- End of Google tag (gtag.js) -->
-    <!--Hostinger test with git -->
-    <!--Hostinger test with git -->
-    <!--Hostinger test with git -->
 </head>
 
 <body <?php body_class(); ?>>
